343f185608215dc71a19d76e59a3f548b43b63c0
Rithufor8 <karthiknone@gmail.com>
2024-08-15 08:41:34 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+function showPage(pageId) {
+    // Hide all pages
+    document.querySelectorAll('.page').forEach(page => {
+        page.style.display = 'none';
+    });
+
+    // Show the selected page
+    document.getElementById(pageId).style.display = 'block';
+}
